e094be0f72da444ff179bc9239f6f9b9e4c0d09a
marsella <marcellahastings@gmail.com>
2014-11-06 14:16:13 -0500

    add page for art homework

--- homepage.py
+++ homepage.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
   return render_template('index.html')
 
+@app.route('/art')
+def art():
+  return render_template('art.html')
+
 if __name__ == "__main__":
   app.debug = True
   app.run(host='0.0.0.0',port=80)
