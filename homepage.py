c655d5f9b29fe64e8846550bdbca63fa2dabbc3c
marsella <marcellahastings@gmail.com>
2015-01-06 15:21:34 -0500

    add blog redirect

--- homepage.py
+++ homepage.py
@@ -10,6 +10,11 @@ def hello():
 def art():
   return render_template('art.html')
 
+@app.route('/blog')
+  return redirect(
+    "http://admissions.tufts.edu/blogs/jumbo-talk/author/marcella-hastings/",
+    code=302)
+
 if __name__ == "__main__":
   app.debug = True
   app.run(host='0.0.0.0',port=80)
